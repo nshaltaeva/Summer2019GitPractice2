1ea1d3e9bcdcf2030a58bebbafbe471a082fd75d
nshaltaeva <nshaltaeva@gmail.com>
2019-12-05 14:54:18 -0500

    first commit

---
+++ src/test/java/java_code/Person.java
@@ -0,0 +1,10 @@
+package java_code;
+
+public class Person {
+    public int age;
+    String firstName;
+    private String address;
+
+    public Person(int age) {this.age=age;
+    }
+}
---
+++ src/test/java/java_code/Student.java
@@ -0,0 +1,8 @@
+package java_code;
+
+
+public class Student extends Person{
+    public Student(int age) {
+        super(age);
+    }
+}
